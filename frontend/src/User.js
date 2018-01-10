7644d218a698ffec54fae44e7f526158804c6924
@rabalyn <n.mundhenke@mailbox.org>
2018-01-10 23:29:31 +0100

    Initial commit User component

---
+++ frontend/src/User.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+  render() {
+    return (
+      <div className="content row">
+        <div className="col-md-1 col-lg-3 col-xl-3"></div>
+        <div className="card col-xs-12 col-sm-12 col-md-10 col-lg-6 col-xl-6">
+          <h5 className="card-header">Login</h5>
+          <div className="card-body">
+            <form>
+              <div className="form-group row">
+                <label htmlFor="username" className="col-5 col-form-label">Accountname</label>
+                <div className="col-7">
+                  <input type="text" className="form-control" id="username" placeholder="gw2account.1234" />
+                </div>
+                <label htmlFor="userpassword" className="col-5 col-form-label">Passwort</label>
+                <div className="col-7">
+                  <input type="password" className="form-control" id="userpassword" placeholder="Passwort" />
+                </div>
+                <div className="col-4"></div>
+                <button type="submit" className="col-4 btn btn-dark btn-block">Anmelden</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default User
