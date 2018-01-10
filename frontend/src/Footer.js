4c238953cb349be7e913a074934c076b63415502
@rabalyn <n.mundhenke@mailbox.org>
2018-01-10 23:26:28 +0100

    Initial commit Footer component

---
+++ frontend/src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <div className="container col-12">
+          <span className="text-muted">Powered by <a href="https://reactjs.org/">react</a>, <a href="https://getbootstrap.com/">bootstrap</a> and <a href="https://nodejs.org/en/">node.js</a> - crafted by <a href="https://twitter.com/rabalyn">@rabalyn</a></span>
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default Footer
