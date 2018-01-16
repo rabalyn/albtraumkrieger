2caeeb5602a88925d4c2269f061c85c66bb13211
@rabalyn <n.mundhenke@mailbox.org>
2018-01-16 20:52:30 +0100

    Add position-fixed to footer

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Footer extends Component {
   render() {
     return (
-      <footer className="footer">
+      <footer className="footer position-fixed">
         <div className="container col-12">
           <span className="text-muted">Powered by <a href="https://reactjs.org/">react</a>, <a href="https://getbootstrap.com/">bootstrap</a> and <a href="https://nodejs.org/en/">node.js</a> - crafted by <a href="https://twitter.com/rabalyn">@rabalyn</a></span>
         </div>
