d1aa856de24244193e4c820e342a682b3517f49d
@rabalyn <n.mundhenke@mailbox.org>
2018-03-23 13:28:52 +0100

    Update css path, add roboto-font, remove serviceWorker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/css/index.css'
+import App from './App'
+import 'typeface-roboto'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+//registerServiceWorker()
