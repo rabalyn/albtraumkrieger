bc58b83dbe4ce48c93bec171a86c9dfafba98fe6
@rabalyn <n.mundhenke@mailbox.org>
2018-03-23 13:21:45 +0100

    Update example config

---
+++ backend/config.example.js
@@ -0,0 +1,12 @@
+const config = {
+  app: {
+    port: 4000,
+    host: 'localhost',
+  },
+  api: {
+    "guildToken": "your-api-token",
+    "guildId": "yout-guild-id"
+  }
+}
+
+module.exports = config
