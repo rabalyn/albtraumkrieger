f9c5ef159b1eb46d0d4dcc6f6efb555a015737cf
@rabalyn <n.mundhenke@mailbox.org>
2018-03-31 15:46:03 +0200

    Add discord link

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import config from './config'
 import githubCat from './imgs/GitHub-Mark/PNG/GitHub-Mark-32px.png'
 import ts3logo from './imgs/teamspeak-logo.svg'
+import discordlogo from './imgs/discord-logo.png'
 
 class Footer extends Component {
   render() {
-    const ts3host = "ts3server://" + config.ts3host + "?port=9987"
+    const confHost = "albha.rabalyn.de" //config.ts3host || "62.104.20.84"
+    const confPort = "9987" //config.ts3port || 10118
+    const ts3host = String().concat("ts3server://", confHost, "?port=", confPort)
+    const discord = config.discord.invitelink || '#'
+
     return (
       <footer className="footer position-fixed">
         <div className="container col-12">
@@ -18,6 +23,9 @@ class Footer extends Component {
 
           <span className="text-muted float-right logolink">
             <a href={ts3host} target="_blank" rel="noopener noreferrer"><img src={ts3logo} alt="TeamSpeak logo" width="30" hspace="3"/></a>
+          </span>
+          <span className="text-muted float-right logolink-spacer logolink">
+            <a href={discord} target="_blank" rel="noopener noreferrer"><img src={discordlogo} alt="Discord logo" width="30" hspace="3"/></a>
           </span>
           <span className="text-muted float-right logolink-spacer logolink">
             <a href="https://github.com/rabalyn/albtraumkrieger" target="_blank" rel="noopener noreferrer"><img src={githubCat} alt="Github logo" hspace="3"/></a>
