7259064e6f9fb05c653f64aafc1507b3c4a7d3ed
WOTAN\rabalyn <n.mundhenke@mailbox.org>
2018-04-14 19:48:23 +0200

    Update model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,29 +1,19 @@
 'use strict'
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const passportLocalMongoose = require('passport-local-mongoose')
 
-module.exports = mongoose.model('User', new Schema({
+const User = new Schema({
   accountid: String,
+  username: {type: String, unique: false},
   apikey: String,
-  password: String,
+  password: {type: String, unique: false},
   email: String,
   joinedAt: Date,
-  rank: String
-}))
+  rank: String,
+  isAdmin: Boolean
+})
 
-/*
-module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    accountid: DataTypes.STRING,
-    apikey: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    joinedAt: DataTypes.DATE,
-    rank: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
-};
-*/
+User.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('User', User)
