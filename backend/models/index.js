54652c8e0df9fb79a36a25e7b44a3676abb6b67f
WOTAN\rabalyn <n.mundhenke@mailbox.org>
2018-04-14 19:49:24 +0200

    Add export of connection for usage in other parts of the App

--- backend/models/index.js
+++ backend/models/index.js
@@ -14,35 +14,6 @@ mongoose.connect('mongodb://'+user+':'+password+'@'+host+':'+port+'/'+database)
 module.exports = {
   User: require('./user'),
   Item: require('./item'),
-  Upgrade: require('./upgrade')
+  Upgrade: require('./upgrade'),
+  Connection: mongoose.connection
 }
-
-/*
-if (config.use_env_variable) {
-  console.log(config)
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
-*/
